41ac3f87cd2111c7db575f9874857b6a56c20d87
sanglee <pksll325@gmail.com>
2020-02-02 14:51:59 +0900

    training clean model on pertubed images

---
+++ art-lib/05jsma_train.py
@@ -0,0 +1,117 @@
+"""
+The script demonstrates a simple example of using ART with PyTorch. The example train a small model on the MNIST dataset
+and creates adversarial examples using the Fast Gradient Sign Method. Here we use the ART classifier to train the model,
+it would also be possible to provide a pretrained model to the ART classifier.
+The parameters are chosen for reduced computational requirements of the script and not optimised for accuracy.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+from art.attacks import FastGradientMethod
+from art.attacks import DeepFool
+from art.attacks import SaliencyMapMethod
+from art.attacks import ProjectedGradientDescent
+from art.classifiers import PyTorchClassifier
+from art.utils import load_mnist
+
+
+# Step 0: Define the neural network model, return logits instead of activation in forward method
+
+class MnistModel(nn.Module):
+    def __init__(self):
+        super(MnistModel, self).__init__()
+        # mnist의 경우 28*28의 흑백이미지(input channel=1)이다.
+        self.conv1 = nn.Conv2d(1, 32, kernel_size = 5, padding=2)
+        # feature map의 크기는 14*14가 된다
+        # 첫번재 convolution layer에서 나온 output channel이 32이므로 2번째 input도 32
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 5, padding=2)
+        # feature map의 크기는 7*7이 된다
+        # fc -> fully connected, fc는 모든 weight를 고려해서 만들기 때문에 cnn에서는 locally connected를 이용하여 만든다.
+        # nn.Linear에서는 conv를 거친 feature map을 1차원으로 전부 바꿔서 input을 한다. 이게 64*7*7
+        self.fc1 = nn.Linear(64*7*7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 64*7*7) # linear에 들어갈 수 있도록 reshape
+        x = F.relu(self.fc1(x)) # fully connected에 relu 적용
+        x = F.dropout(x, training=self.training) # 가중치 감소만으로는 overfit을 해결하기가 어려움, 그래서 뉴런의 연결을 임의로 삭제
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+# Step 1: Load the MNIST dataset
+
+(x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = load_mnist()
+
+# Step 1a: Swap axes to PyTorch's NCHW format
+
+x_train = np.swapaxes(x_train, 1, 3).astype(np.float32)
+x_test = np.swapaxes(x_test, 1, 3).astype(np.float32)
+
+# Step 2: Load the model
+
+is_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if is_cuda else 'cpu')
+
+pretrained_model = './model/mnist_um_art.pth'
+model = MnistModel().to(device)
+model.load_state_dict(torch.load(pretrained_model, map_location=device))
+
+
+# Step 2a: Define the loss function and the optimizer
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+# Step 3: Create the ART classifier
+
+classifier = PyTorchClassifier(model=model, clip_values=(min_pixel_value, max_pixel_value), loss=criterion,
+                               optimizer=optimizer, input_shape=(1, 28, 28), nb_classes=10)
+
+
+# Step 4: Evaluate the ART classifier on benign test examples
+
+predictions = classifier.predict(x_test)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+print('Accuracy on benign test examples: {}%'.format(accuracy * 100))
+
+# Step 5: Generate adversarial test examples
+
+attack = SaliencyMapMethod(classifier=classifier)
+x_test_adv = attack.generate(x=x_test)
+x_train_adv = attack.generate(x=x_train)
+
+# Step 6: Evaluate the ART classifier on adversarial test examples
+
+predictions = classifier.predict(x_test_adv)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+print('Accuracy on clean model with jsma test examples: {}%'.format(accuracy * 100))
+
+# Step 7: Train clean model with perturbed images
+
+print("\nTraining with adversarial example...\n")
+classifier.fit(x_train_adv, y_train, batch_size=64, nb_epochs=30)
+
+# Step 8: Evaluate the new ART classifier on benign test examples
+
+predictions = classifier.predict(x_test)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+print('Accuracy on benign test examples: {}%\n'.format(accuracy * 100))
+
+
+# Step 9: Evaluate the new ART classifier on adversarial test examples
+
+predictions = classifier.predict(x_test_adv)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+print('Accuracy on new model with jsma test examples: {}%'.format(accuracy * 100))
+
+# Step 10: Save trained model
+
+PATH = './model/mnist_jsma_art.pth'
+torch.save(model.state_dict(), PATH)
+
